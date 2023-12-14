b487b6742e4ef06a1de0a54987194a83d4310e25
Wasiu-hub <contactbow@yahoo.com>
2023-12-14 17:19:18 +0000

    last commit further refactored

--- src/test/java/com/next/steps/Search.java
+++ src/test/java/com/next/steps/Search.java
@@ -28,8 +28,8 @@ public class Search {
     }
 
     @When("I enter {string} as a product type in the search box")
-    public void i_enter_as_a_product_type_in_the_search_box(String string) {
-        driver.findElement(By.xpath("//input[@id='header-big-screen-search-box']")).sendKeys(string);
+    public void i_enter_as_a_product_type_in_the_search_box(String prototype) {
+        driver.findElement(By.xpath("//input[@id='header-big-screen-search-box']")).sendKeys(prototype);
     }
 
     @When("I click on search button")
@@ -38,9 +38,9 @@ public class Search {
     }
 
     @Then("I should be able to see {string} as a result title")
-    public void i_should_be_able_to_see_as_a_result_title(String string) {
+    public void i_should_be_able_to_see_as_a_result_title(String resultTitle) {
 
-        String expectedResult = "\"Jeans\"";
+        String expectedResult = "\"" + resultTitle + "\"";
 //        Thread.sleep(200);// needed for safari browser
         String actualResult = driver.findElement(By.xpath("//*[@id=\"plp-results-title-container\"]/div/h1/span")).getText();
 
