4d13160e60d10a579f1e46b671789d90c4097511
Wasiu-hub <contactbow@yahoo.com>
2023-12-14 19:21:25 +0000

    shows escape character in Java

---
+++ src/test/java/com/next/steps/Demo.java
@@ -0,0 +1,12 @@
+package com.next.steps;
+
+public class Demo {
+    public static void main(String[] args) {
+//        System.out.println("Jeans"); //Jeans
+
+//        String resultTitle = "James\"";
+//        System.out.println(resultTitle); // James"
+
+    }
+
+}
