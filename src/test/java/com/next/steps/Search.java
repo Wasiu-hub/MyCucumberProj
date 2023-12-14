cd7e1c837acdd90187d76f306d7bb5aaa7ec2a6c
Wasiu-hub <contactbow@yahoo.com>
2023-12-14 13:12:04 +0000

    step definition file refactored

--- src/test/java/com/next/steps/Search.java
+++ src/test/java/com/next/steps/Search.java
@@ -9,22 +9,14 @@ public class Search {
 
     @Given("I am on the next home page")
     public void i_am_on_the_next_home_page() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
     }
     @When("I enter {string} as a product type in the search box")
     public void i_enter_as_a_product_type_in_the_search_box(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
     }
     @When("I click on search button")
     public void i_click_on_search_button() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
     }
     @Then("I should be able to see {string} as a result title")
     public void i_should_be_able_to_see_as_a_result_title(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
     }
 }
