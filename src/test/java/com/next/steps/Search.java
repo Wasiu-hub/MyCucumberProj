4406ddb6f84a0256cb04a01ee8a6b94b8d0e2912
Wasiu-hub <contactbow@yahoo.com>
2023-12-14 15:05:12 +0000

    When: enter the Jeans input

--- src/test/java/com/next/steps/Search.java
+++ src/test/java/com/next/steps/Search.java
@@ -13,22 +13,28 @@ import java.time.Duration;
 // contains all search related step definitions
 public class Search {
 
+    WebDriver driver;
+
     @Given("I am on the next home page")
     public void i_am_on_the_next_home_page() {
         WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
+        driver = new ChromeDriver();
 //        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));// this needed for safari browser
         driver.manage().window().maximize();
         driver.get("https://www.next.co.uk");
         driver.findElement(By.id("onetrust-accept-btn-handler")).click();
 
     }
+
     @When("I enter {string} as a product type in the search box")
     public void i_enter_as_a_product_type_in_the_search_box(String string) {
+        driver.findElement(By.xpath("//input[@id='header-big-screen-search-box']")).sendKeys("Jeans");
     }
+
     @When("I click on search button")
     public void i_click_on_search_button() {
     }
+
     @Then("I should be able to see {string} as a result title")
     public void i_should_be_able_to_see_as_a_result_title(String string) {
     }
