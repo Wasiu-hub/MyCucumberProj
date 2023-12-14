8b52bae6436ae5d00278e0cb8559b61c024101e3
Wasiu-hub <contactbow@yahoo.com>
2023-12-14 20:44:27 +0000

    dryRun implemented to validate or invalidate our tests

--- src/test/java/com/next/NextRunner.java
+++ src/test/java/com/next/NextRunner.java
@@ -5,6 +5,6 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = ".")
+@CucumberOptions(features = ".", dryRun = true)
 public class NextRunner {
 }
