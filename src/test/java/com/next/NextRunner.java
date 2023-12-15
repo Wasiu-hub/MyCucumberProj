13596fee5f0e58c33e511bf2e418010f6c474eb5
Wasiu-hub <contactbow@yahoo.com>
2023-12-15 01:56:10 +0000

    generating test results using cucmber framework

--- src/test/java/com/next/NextRunner.java
+++ src/test/java/com/next/NextRunner.java
@@ -6,6 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 //@CucumberOptions(features = ".", dryRun = true)
-@CucumberOptions(features = ".", dryRun = false, tags = "@SmokeTest")
+@CucumberOptions(features = ".", dryRun = false, tags = "@SmokeTest", plugin = {"pretty", "html:reports.html"})
 public class NextRunner {
+
 }
