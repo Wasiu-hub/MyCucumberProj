86e85a804b28b1e53697dda05fe8db4545d1abb3
Wasiu-hub <contactbow@yahoo.com>
2023-12-15 02:29:37 +0000

    running specific feature file by file path

--- src/test/java/com/next/NextRunner.java
+++ src/test/java/com/next/NextRunner.java
@@ -6,8 +6,12 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 //@CucumberOptions(features = ".", dryRun = true)
-@CucumberOptions(features = ".", dryRun = false,
-//        tags = "@SmokeTest",
+@CucumberOptions(
+
+//      features = ".",
+        features = "src/test/java/com/next/feature/search.feature",
+        dryRun = false,
+//      tags = "@SmokeTest",
         plugin = {"pretty", "html:reports.html"})
 public class NextRunner {
 
